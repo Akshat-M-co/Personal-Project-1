7098ab493014c97ab27573fdc26bd1b2b2b174fa
Akshat-M-co <akshatmittal0910@gmail.com>
2026-04-16 12:30:39 +0000

    Added save system.

---
+++ save.py
@@ -0,0 +1,31 @@
+from act import *
+auto_save = False
+def save_game(details, save_slot):
+    if auto_save:
+        with open(f"Save file {save_slot}.txt", "w") as save:
+            for i in details:
+                save.write(i)
+                save.write("\n")
+                print("Save complete. Turn off autosave in the settings, if not wanted.")
+    else:
+        print("Are you sure you wish to save the game? (y/n)")
+        ans = input("\n")
+        if ans.lower() == 'y':
+            print("Saving your game.")
+            with open(f"Save file {save_slot}.txt", "w") as save:
+            for i in player_details:
+                save.write(i)
+                save.write("\n")
+            print("Save complete.")
+        else:
+            print("Exitting save mode...")
+    return
+def load_game(save_slot):
+    load = open(f"Save file {save_slot}", "r")
+    r = load.readline()
+    details = []
+    while (r):
+        details.append(r)
+    load.close()
+    print("Loading complete.")
+    return details
