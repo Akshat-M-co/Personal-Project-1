02c72f091f746edeb21978857b568d88dce7fe22
Akshat-M-co <akshatmittal0910@gmail.com>
2026-04-18 08:56:36 +0000

    tutorial beginning

---
+++ tutorial.py
@@ -0,0 +1,10 @@
+#tutorial for the beginning.
+def gent():
+    print("================GENERAL TUTORIAL===================")
+    print("A.\tCharacters")
+    print("In Warrior's Quest, you have 4 major classes to choose from: Fighter, Barbarian, Mage and Healer.\nEach have their own unique strengths and weaknesses.You can find their descriptions below:")
+    print("1.\tFighter\n")
+    print("A soldier-like warrior, with great balance of offence and defence, and good weapon proficiency in all weapons. Has great agility, but is completely lacking in magical potential and strength.\n Has the special ablity FOCUS, which improves chances of hitting target by 100%.")
+    print("2.\tBarbarian\n")
+    print("A merciless warrior, who focuses mainly on attack and damage, but cares very little for defence and agility. Can use magic to improve damage potential and power.")
+    print("Has the special ability RAGE, which improves his damage by 50%.")
